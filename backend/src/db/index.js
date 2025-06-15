a00a4db99b99668d8cf25ff351af491adb1cd2e1
rutujamavkar@gmail.com <rutujamavkar.in>
2025-06-16 00:07:22 +0530

    database connection done successfully

---
+++ backend/src/db/index.js
@@ -0,0 +1,14 @@
+import {DB_NAME} from '../constants.js'
+import mongoose from 'mongoose'
+
+const ConnectDB=async()=>{
+    try{
+        const connectionInstance=await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        console.log(`mongodb connected successfully!!!!!! DBHOST:${ connectionInstance.connection.host}`)
+    }
+    catch(error){
+        console.log("error in database connection", error)
+    }
+    
+}
+export default ConnectDB
---
+++ backend/src/index.js
@@ -0,0 +1,18 @@
+import {app} from './app.js'
+import connectDB from './db/index.js'
+import dotenv from 'dotenv'
+
+
+dotenv.config({
+    path:'./.env'
+})
+
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT,()=>{
+        console.log("The database is connected at localhost:",process.env.PORT)
+    })
+})
+.catch((error)=>{
+    console.log("Error in database connection", error)
+})
